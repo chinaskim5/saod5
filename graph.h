377f06536a6de249f88388e04770a429e7c100f1
Chinaski <emindanew@yandex.ru>
2017-05-20 01:43:29 +0800

    graph

---
+++ graph.h
@@ -0,0 +1,19 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <stdlib.h>
+
+struct graph {
+	int nvertices;
+	int *m;
+	int *visited;
+};
+
+struct graph *graph_create(int vertices);
+void graph_clear(struct graph *g);
+void graph_free(struct graph *g);
+void graph_set_edge(struct graph *g, int i, int j, int w);
+int graph_get_edge(struct graph *g, int i, int j);
+
+void graph_print(graph *g);
+#endif
